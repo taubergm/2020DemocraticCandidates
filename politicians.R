ea40cea009457b5812998e9b0f83a9aad0c45a61
michael tauberg <michaeltauberg@michaels-MacBook-Pro.local>
2019-04-29 22:16:26 -0700

    updates on chart styles

--- politicians.R
+++ politicians.R
@@ -40,7 +40,7 @@ pacman::p_load(sentimentr, dplyr, magrittr)
 library('sentimentr')
 
 workingDir = '/Users/michaeltauberg/projects/NewsScraper'
-csvName = "politicians4.csv"
+csvName = "politicians7.csv"
 data_name = "politicians"
 setwd(workingDir)
 #options(encoding="latin1")
@@ -58,9 +58,11 @@ all = c( "politico","huff","buzzfeed", "cnn", "msnbc", "vox", "npr",
         "foxnews", "dailycaller", "infowars", "breitbart")
 
 online = c("cnn", "msnbc", "npr", "vox")
-liberal = c("npr", "politico","huff","buzzfeed", "cnn", "msnbc", "vox", "npr", "nytimes")
+
+#https://www.journalism.org/2014/10/21/political-polarization-media-habits/
+liberal = c("npr", "politico","huff","buzzfeed", "cnn", "msnbc", "nytimes")
 newspapers = c("nytimes", "wsj", "latimes", "usatoday", "washingtonpost", "bostonglobe")
-conservative = c("foxnews", "dailycaller", "infowars", "breitbart")
+conservative = c("foxnews", "dailycaller", "breitbart")
 cnn = c("cnn") # seems too low
 nytimes = c("nytimes")
 
@@ -72,19 +74,23 @@ politicians = c("harris", "warren", "sanders", "yang", "biden", "beto", "booker"
 
 # remove stories not about politicians- ie warren buffet stories (not elizabeth warren)
 dt = dt[!grepl("Buffet", dt$content),]
-dt = dt[!grepl("buffet", dt$content),]
-dt = dt[!grepl("Harrison", dt$headline),]
+dt = dt[!grepl("buffet", dt$headline),]
+dt = dt[!grepl("harrison", dt$headline),]
 dt = dt[!grepl("Harrison", dt$content),]
 dt = dt[!grepl("Joe Harris", dt$headline),]
 dt = dt[!grepl("joe harris", dt$content),]
-dt = dt[!grepl("Fidel", dt$headline),]
+dt = dt[!grepl("fidel", dt$headline),]
 dt = dt[!grepl("Fidel", dt$content),]
 dt = dt[!grepl("Raul", dt$content),]
-dt = dt[!grepl("Huckabee", dt$headline),]
+dt = dt[!grepl("raul", dt$headline),]
+dt = dt[!grepl("huckabee", dt$headline),]
 dt = dt[!grepl("Huckabee", dt$content),]
-dt = dt[!grepl("Aubameyang", dt$headline),]
+dt = dt[!grepl("aubameyang", dt$headline),]
 dt = dt[!grepl("Aubameyang", dt$content),]
-
+dt = dt[!grepl("pyongyang", dt$headline),]
+dt = dt[!grepl("hengjun", dt$headline),]
+dt = dt[!grepl("yangtze", dt$headline),]
+dt = dt[!grepl("yang-ho", dt$headline),]
 
 
 # wee need a way to find if more than on is mentioned . If more than one, add it to the first one
@@ -132,16 +138,20 @@ ComparePoliticians <- function(dt, politicians, news_category, name) {
     i = i + 1
   }
   num_source_articles$num_articles = strtoi(num_source_articles$num_articles)
-  num_source_articles$politician = factor(num_source_articles$politician, levels = num_source_articles$politician[order(num_source_articles$num_articles, decreasing=TRUE)])
+  num_source_articles$politician = factor(num_source_articles$politician, levels = num_source_articles$politician[order(num_source_articles$num_articles, decreasing=FALSE)])
   num_source_articles = num_source_articles[order(num_source_articles$num_articles, decreasing=TRUE),]
-  p = ggplot(num_source_articles, aes(x=politician, y=num_articles, fill=politician)) + geom_bar(stat="identity") 
+  p = ggplot(num_source_articles, aes(x=politician, y=num_articles, fill="#003b9b")) + geom_bar(stat="identity") 
   p = p + ggtitle(sprintf("%s News Articles per Politician", name))
   p = p + theme(plot.title = element_text(hjust = 0.5))
-  p = p + theme(axis.text.x=element_text(angle=90, hjust=1,face="bold"))
-  p = p + theme(axis.text=element_text(size=16,face="bold"), axis.title=element_text(size=13), axis.title.x=element_blank())
+  #p = p + theme(axis.text.x=element_text(angle=90, hjust=1,face="bold"))
+  p = p + theme(axis.text=element_text(size=16,face="bold"), axis.title=element_text(size=13), axis.title.y=element_blank())
   p = p + theme(plot.title = element_text(size=18,face="bold"))
   #p = p + scale_fill_manual(values = c(color)) + guides(fill=FALSE)
+  #p = p + xlab("Number of Articles") + guides(fill=FALSE)
   p = p + ylab("Number of Articles") + guides(fill=FALSE)
+  p = p + coord_flip()
+  p = p + scale_fill_manual(values = c("#003b9b"))
+  #p = p + scale_fill_manual(values = c("#ad1605"))
   ggsave(filename = sprintf("./%s_articles.png", name) , plot=p, width=8, height=6)
 }
 
@@ -235,6 +245,13 @@ castro = dt[grep("castro", dt$headline),]
 gabbard = dt[grep("gabbard", dt$headline),]
 buttigieg = dt[grep("buttigieg", dt$headline),]
 
+yang = yang[!grepl("aubameyang", yang$headline),]
+yang = yang[!grepl("Aubameyang", yang$headline),]
+yang = yang[!grepl("pyongyang", yang$headline),]
+yang = yang[!grepl("hengjun", yang$headline),]
+yang = yang[!grepl("yangtze", yang$headline),]
+yang = yang[!grepl("yang-ho", yang$headline),]
+
 GenerateWordClouds(harris, "harris", "blue")
 GenerateWordClouds(warren, "warren", "blue")
 GenerateWordClouds(sanders, "sanders", "blue")
@@ -253,12 +270,12 @@ GenerateWordClouds(booker, "booker", "blue")
 #
 ####-------------
 
-PlotOverTime <- function(dt, name, weekly_article_stats, max_date) {
-  name.ts = xts(dt$num_articles,dt$date)
+PlotOverTime <- function(dt_candidate, name, weekly_article_stats, max_date) {
+  name.ts = xts(dt_candidate$num_articles,dt_candidate$date)
   articles = apply.weekly(name.ts,sum)
-  articles = articles[1:length(articles)-1] # remove last entry for incomplete week
+  articles = articles[1:length(articles)] # remove last entry for incomplete week
   #dev.on()
-  png(sprintf('./%s_time.png', name))
+  png(sprintf('./%s_time.png', name), width = 880, height = 580,)
   print(plot(articles))
   #dev.copy(png, sprintf('%s_time.png', name))
   dev.off()
@@ -275,7 +292,7 @@ PlotOverTime <- function(dt, name, weekly_article_stats, max_date) {
   return(weekly_article_stats)
 }
 
-max_date = "2019-04-8"
+max_date = "2019-04-26"
 weekly_article_stats = data.frame()
 weekly_article_stats = PlotOverTime(sanders, "sanders", weekly_article_stats, max_date)
 weekly_article_stats = PlotOverTime(harris, "harris", weekly_article_stats, max_date)
@@ -308,15 +325,15 @@ gillibrand = weekly_article_stats[weekly_article_stats$politician == "gillibrand
 klobuchar = weekly_article_stats[weekly_article_stats$politician == "klobuchar", ]
 buttigieg = weekly_article_stats[weekly_article_stats$politician == "buttigieg", ]
 
-top_contenders = rbind(warren,harris,sanders, beto, biden)
+beto$politician = rep("o'rourke", nrow(beto))
+top_contenders = rbind(warren,harris,sanders, beto, biden, buttigieg)
 top_contenders$politician = factor(top_contenders$politician)
 
 data_name = "top_contenders"
-p = ggplot(top_contenders, aes(x=date, y=num_articles, color=politician)) + geom_point() 
-p = p + geom_line()
+p = ggplot(top_contenders, aes(x=date, y=num_articles, color=politician)) +  geom_line(aes(linetype = politician))
 #p = p + stat_smooth(aes(x =date, y =num_articles), method = "lm",
 #              formula = y ~ poly(x, floor(nrow(top_contenders)/length(levels(top_contenders$politician)))-2), se = FALSE) 
-p = p + ggtitle(sprintf("%s - Num Articles Over Time", toupper(data_name)))
+p = p + ggtitle("Frontrunners Num Articles Over Time")
 p = p + theme(plot.title = element_text(hjust = 0.5))
 p = p + theme(axis.text.x=element_text(angle=90, hjust=1,face="bold"))
 p = p + theme(axis.text=element_text(size=16,face="bold"), axis.title=element_text(size=13), axis.title.x=element_blank())
@@ -327,7 +344,7 @@ p = p + scale_y_continuous(limits = c(0, max(top_contenders$num_articles) + 10))
 ggsave(filename = sprintf("./%s_all_over_time.png", data_name) , plot=p, width=8, height=6)
 
 
-bottom_contenders = rbind(yang, booker, gillibrand, klobuchar, buttigieg)
+bottom_contenders = rbind(yang, booker, gillibrand, klobuchar, castro, gabbard)
 bottom_contenders$politician = factor(bottom_contenders$politician)
 data_name = "bottom_contenders"
 p = ggplot(bottom_contenders, aes(x=date, y=num_articles, color=politician)) + geom_point() 
@@ -372,19 +389,23 @@ colnames(sentiments) = c("politician", "avg_sentiment")
 sentiments = data.frame(sentiments)
 sentiments$avg_sentiment = as.numeric(as.character(sentiments$avg_sentiment))
 data_name = "liberal_sentiment"
-sentiments$politician = factor(sentiments$politician, levels = sentiments$politician[order(sentiments$avg_sentiment, decreasing=TRUE)])
+sentiments$politician = factor(sentiments$politician, levels = sentiments$politician[order(sentiments$avg_sentiment, decreasing=FALSE)])
 sentiments = sentiments[order(sentiments$avg_sentiment, decreasing=TRUE),]
-p = ggplot(sentiments, aes(x=politician, y=avg_sentiment, fill=politician)) + geom_bar(stat="identity") 
+p = ggplot(sentiments, aes(x=politician, y=avg_sentiment, fill="#003b9b")) + geom_bar(stat="identity") 
 p = p + ggtitle("Average Story Sentiment per Candidate from Liberal News Sources")
 p = p + theme(plot.title = element_text(hjust = 0.5))
 p = p + theme(axis.text.x=element_text(angle=90, hjust=1,face="bold"))
-p = p + theme(axis.text=element_text(size=16,face="bold"), axis.title=element_text(size=13), axis.title.x=element_blank())
+p = p + theme(axis.text=element_text(size=16,face="bold"), axis.title=element_text(size=13), axis.title.y=element_blank())
 p = p + theme(plot.title = element_text(size=14,face="bold"))
 #p = p + scale_fill_manual(values = c(color)) + guides(fill=FALSE)
-p = p + ylab("Avg Sentiment Score") + guides(fill=FALSE)
+p = p + xlab("Avg Sentiment Score") + guides(fill=FALSE)
+p = p + coord_flip()
+p = p + scale_fill_manual(values = c("#003b9b"))
 ggsave(filename = sprintf("./%s_sentiment.png", data_name) , plot=p, width=8, height=6)
 
 politician_sentiments = c()
+politicians = c("harris", "warren", "sanders", "biden", "beto", "booker",
+                "gillibrand", "klobuchar", "castro", "gabbard", "buttigieg")
 for (politician in politicians) {
   print(politician)
   politician_articles = liberal_articles[grep(politician, liberal_articles$headline),]
@@ -397,18 +418,19 @@ colnames(sentiments) = c("politician", "avg_sentiment")
 sentiments = data.frame(sentiments)
 sentiments$avg_sentiment = as.numeric(as.character(sentiments$avg_sentiment))
 data_name = "liberal_headline_sentiment"
-sentiments$politician = factor(sentiments$politician, levels = sentiments$politician[order(sentiments$avg_sentiment, decreasing=TRUE)])
+sentiments$politician = factor(sentiments$politician, levels = sentiments$politician[order(sentiments$avg_sentiment, decreasing=FALSE)])
 sentiments = sentiments[order(sentiments$avg_sentiment, decreasing=TRUE),]
-p = ggplot(sentiments, aes(x=politician, y=avg_sentiment, fill=politician)) + geom_bar(stat="identity") 
+p = ggplot(sentiments, aes(x=politician, y=avg_sentiment, fill="#003b9b")) + geom_bar(stat="identity") 
 p = p + ggtitle("Average Headline Sentiment per Candidate from Liberal News Sources")
 p = p + theme(plot.title = element_text(hjust = 0.5))
 p = p + theme(axis.text.x=element_text(angle=90, hjust=1,face="bold"))
-p = p + theme(axis.text=element_text(size=16,face="bold"), axis.title=element_text(size=13), axis.title.x=element_blank())
+p = p + theme(axis.text=element_text(size=16,face="bold"), axis.title=element_text(size=13), axis.title.y=element_blank())
 p = p + theme(plot.title = element_text(size=14,face="bold"))
 #p = p + scale_fill_manual(values = c(color)) + guides(fill=FALSE)
-p = p + ylab("Avg Sentiment Score") + guides(fill=FALSE)
+p = p + xlab("Avg Sentiment Score") + guides(fill=FALSE)
+p = p + coord_flip()
+p = p + scale_fill_manual(values = c("#003b9b"))
 ggsave(filename = sprintf("./%s_sentiment.png", data_name) , plot=p, width=8, height=6)
-
 
 # get only articles from conservative sources and do sentiment
 politician_sentiments = c()
@@ -478,17 +500,21 @@ new_followers = read.csv("politician_twitter_newfollowers.csv")
 new_followers = new_followers[new_followers$politician != "trump",]
 new_followers = new_followers[new_followers$politician != "aoc",]
 #new_followers = new_followers[new_followers$politician != "buttigieg",]
+#new_followers[new_followers$date == 2,]$date = "Feb" 
+#new_followers[new_followers$date == 3,]$date = "March" 
+#new_followers[new_followers$date == 4,]$date = "April" 
 data_name = "new_twitter_followers"
 p = ggplot(new_followers, aes(x=date, y=delta, color=politician)) + geom_point() 
 p = p + geom_line()
-p = p + ggtitle("Candidates New Twitter Followers per Month (millions)")
+p = p + ggtitle("New Twitter Followers per Month (millions)")
 p = p + theme(plot.title = element_text(hjust = 0.5))
 p = p + theme(axis.text.x=element_text(angle=90, hjust=1,face="bold"))
-p = p + theme(axis.text=element_text(size=16,face="bold"), axis.title=element_text(size=13), axis.title.x=element_blank())
+p = p + theme(axis.text=element_text(size=16,face="bold"), axis.title=element_text(size=13))
 p = p + theme(plot.title = element_text(size=18,face="bold"))
 p = p + ylab("New Twitter Followers (millions)")
 p = p + xlab("Month")
 p = p + scale_x_continuous(breaks = c(2,3,4))
+#p + scale_x_discrete(labels=c("2" = "Feb", "3" = "March", "4" = "April"))
 ggsave(filename = sprintf("./%s_all_over_time.png", data_name) , plot=p, width=8, height=6)
 
 data_name = "total_twitter_followers"
@@ -508,3 +534,70 @@ p = p + xlab("Candidate")
 ggsave(filename = sprintf("./%s_articles.png", data_name) , plot=p, width=8, height=6)
 
 
+####-------------
+#
+#
+# Polls
+#
+#
+####-------------
+data_name = "polls"
+polls = read.csv("polls.csv")
+polls$name = factor(polls$name, levels = polls$name[order(polls$poll, decreasing=FALSE)])
+p = ggplot(polls, aes(x=name, y=poll, fill="#42b0f4")) + geom_bar(stat="identity") 
+p = p + ggtitle("Polling Among Democratic Primary Voters")
+p = p + theme(plot.title = element_text(hjust = 0.5))
+#p = p + theme(axis.text.x=element_text(angle=90, hjust=1,face="bold"))
+p = p + theme(axis.text=element_text(size=16,face="bold"), axis.title=element_text(size=13), axis.title.y=element_blank())
+p = p + theme(plot.title = element_text(size=18,face="bold"))
+#p = p + scale_fill_manual(values = c(color)) + guides(fill=FALSE)
+#p = p + xlab("Number of Articles") + guides(fill=FALSE)
+p = p + ylab("Poll %") + guides(fill=FALSE)
+p = p + coord_flip()
+p = p + scale_fill_manual(values = c("#42b0f4"))
+ggsave(filename = sprintf("./%s_articles.png", data_name) , plot=p, width=8, height=6)
+
+
+
+
+####-------------
+#
+#
+# Facebook
+#
+#
+####-------------
+
+# new followers
+new_followers = read.csv("politician_facebook_newfollowers.csv")
+#new_followers = new_followers[new_followers$politician != "buttigieg",]
+#new_followers[new_followers$date == 2,]$date = "Feb" 
+#new_followers[new_followers$date == 3,]$date = "March" 
+#new_followers[new_followers$date == 4,]$date = "April" 
+data_name = "new_facebook_followers"
+p = ggplot(new_followers, aes(x=month, y=new_followers, color=name)) + geom_point() 
+p = p + geom_line()
+p = p + ggtitle("New Facebook Followers per Month (millions)")
+p = p + theme(plot.title = element_text(hjust = 0.5))
+p = p + theme(axis.text.x=element_text(angle=90, hjust=1,face="bold"))
+p = p + theme(axis.text=element_text(size=16,face="bold"), axis.title=element_text(size=13))
+p = p + theme(plot.title = element_text(size=18,face="bold"))
+p = p + ylab("New Facebook Followers")
+p = p + xlab("Month")
+p = p + scale_x_continuous(breaks = c(2,3,4))
+#p + scale_x_discrete(labels=c("2" = "Feb", "3" = "March", "4" = "April"))
+ggsave(filename = sprintf("./%s_all_over_time.png", data_name) , plot=p, width=8, height=6)
+
+data_name = "total_facebook_followers"
+followers = read.csv("politician_facebook_totalfollowers.csv")
+followers$politician = factor(followers$politician, levels = followers$politician[order(followers$followers, decreasing=TRUE)])
+followers = followers[order(followers$followers, decreasing=TRUE),]
+p = ggplot(followers, aes(x=politician, y=followers, fill=politician)) + geom_bar(stat="identity") 
+p = p + ggtitle("Candidates Total Facebook Followers in April 2019 (millions)")
+p = p + theme(plot.title = element_text(hjust = 0.5))
+p = p + theme(axis.text.x=element_text(angle=90, hjust=1,face="bold"))
+p = p + theme(axis.text=element_text(size=16,face="bold"), axis.title=element_text(size=13), axis.title.x=element_blank())
+p = p + theme(plot.title = element_text(size=18,face="bold"))
+p = p + ylab("Total Facebook Followers")
+p = p + xlab("Candidate")
+ggsave(filename = sprintf("./%s_articles.png", data_name) , plot=p, width=8, height=6)
